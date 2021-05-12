c79432e967ed0199dca5c8d761c18923f5355eec
DForeman <dlydellf@hotmail.com>
2021-05-12 16:57:59 -0500

    Moved minor assignments into their own folder

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-alert("Hello, world!  I am an alert box!");
-
-function My_First_Function() {
-		var String = "Kiss me, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML = result;
-	}
