9ef25c0516602b173cc8b71012c36083de0884ee
DForeman <dlydellf@hotmail.com>
2021-05-27 00:54:23 -0500

    Created a JS folder; moved .html scripts to defer the JS & avoid an 'Uncaught Reference Error

---
+++ Basic_HTML_and_CSS/Bootstrap4_Project(Pool _Academy_Website)/Bootstrap JS/main.js
@@ -0,0 +1,14 @@
+function bookmarkHue(NavbarClass) {
+  if (NavbarClass == "Beginner") {
+    hue = "green";
+  } else if (NavbarClass == "Intermediate") {
+    hue = "white";
+  } else hue = "red";
+  bookmark(NavbarClass, hue);
+}
+
+function bookmark(NavbarClass, hue) {
+  document.getElementById(`${NavbarClass}Image`).style.boxShadow = `1px 1px 15px 10px ${hue}`;
+  document.getElementById(`${NavbarClass}Image`).style.backgroundColor = `${hue}`;
+  document.getElementById(`${NavbarClass}Text`).style.border = `1px solid ${hue}`;
+}
