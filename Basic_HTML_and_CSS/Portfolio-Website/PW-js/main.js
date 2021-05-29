ee2271045ae589ca5cadd74a6fc0c1a9dd9ec90d
DForeman <dlydellf@hotmail.com>
2021-05-29 01:20:17 -0500

    Created validateForm()

---
+++ Basic_HTML_and_CSS/Portfolio-Website/PW-js/main.js
@@ -0,0 +1,9 @@
+function validateForm() {
+  var inputs = document.forms["contactForm"]
+  if (inputs[type="text"].value || inputs[type="email"].value = "") {
+    alert("Please fill in all of form's fields before submitting")
+  }
+  if (!inputs[type="email"].value.contains("@")) {
+    alert("Please enter a valid email address")
+  }
+}
