2074210b39dac2ec277da6dd1ce8efe71debaa4e
DForeman <dlydellf@hotmail.com>
2021-05-29 13:59:44 -0500

    Added a console.log; simplified 1st conditional, commented-out 2nd conditional - still not 100% g2g

--- Basic_HTML_and_CSS/Portfolio-Website/PW-js/main.js
+++ Basic_HTML_and_CSS/Portfolio-Website/PW-js/main.js
@@ -1,10 +1,10 @@
 function validateForm() {
   var inputs = document.forms["contactForm"];
-  console.log(inputs);
-  if ((inputs[(type = "text")].value || inputs[(type = "email")].value) == "") {
+  console.log(inputs((type = "email")));
+  if (inputs[(type = "text")].value == "" || inputs[(type = "email")].value == "") {
     alert("Please fill in all of form's fields before submitting");
-  }
-  if (!inputs[(type = "email")].value.contains("@")) {
+  } /*
+  if (!inputs[(type = "email")].value.contains("@")) { // Will never work / <form>'s "type=email" overrides this???
     alert("Please enter a valid email address");
-  }
+  }*/
 }
